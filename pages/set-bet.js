7fcf95903ca72feca9fc6782e180777e827a1c11
amatureApe <amatureApe@gmail.com>
2022-11-17 17:13:20 -0800

    more minor changes

--- pages/set-bet.js
+++ pages/set-bet.js
@@ -4,18 +4,22 @@ import {
     Textarea,
     Input,
     Button,
+    Box,
+    Flex,
     FormLabel,
     FormControl,
     Radio,
     RadioGroup,
     Stack,
     Text,
+    Spacer,
     NumberInput,
     NumberInputField,
     NumberInputStepper,
     NumberIncrementStepper,
     NumberDecrementStepper,
 } from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
 import Layout from '../components/layout/article'
 import React from 'react'
@@ -31,7 +35,13 @@ const Works = () => {
                     Set Your Bet!
                 </Heading>
             </Container>
-            <Heading>Your Bet</Heading>
+            <Stack justify="space-between" direction="row" align="end">
+                <Heading>Your Bet</Heading>
+                <Stack justify="space-between" align="center" direction="row">
+                    <Button m={2} variant="ghost" colorScheme="pink">Need Help?</Button>
+                    <Button variant="outline" colorScheme="pink">Advanced<ChevronDownIcon /></Button>
+                </Stack>
+            </Stack>
             <Textarea bg="whiteAlpha.800" mb={4} _placeholder={{ color: "#525252" }} placeholder="What do you want to bet?" />
             <Heading>Collateral</Heading>
             <Input bg="whiteAlpha.800" mb={4} _placeholder={{ color: "#525252" }} placeholder="Input your collateral token here" />
@@ -58,22 +68,28 @@ const Works = () => {
             <Input bg="whiteAlpha.800" mb={4} _placeholder={{ color: "#525252" }} placeholder="Input your reward to validators here" />
             <Heading>Liveness Period</Heading>
             <Input bg="whiteAlpha.800" mb={4} _placeholder={{ color: "#525252" }} placeholder="Input your Livness Period here" />
-            <Heading>Bet Privacy</Heading>
-            <Text mb={1}>Is your bet public or private?</Text>
-            <RadioGroup onChange={setBetPrivacy} value={betPrivacy} mb={4}>
-                <Stack direction='row'>
-                    <Radio value='1' colorScheme="pink">Public</Radio>
-                    <Radio value='2' colorScheme="pink">Private</Radio>
-                </Stack>
-            </RadioGroup>
-            <Heading>Bet Side</Heading>
-            <Text mb={1}>Are you in affirmation or negation of the bet?</Text>
-            <RadioGroup onChange={setBetSide} value={betSide} mb={4}>
-                <Stack direction='row'>
-                    <Radio value='1' colorScheme="pink">Affirmation</Radio>
-                    <Radio value='2' colorScheme="pink">Negation</Radio>
-                </Stack>
-            </RadioGroup>
+            <Flex direction="row" justify="space-between">
+                <Flex direction="column" justify="center" align="center">
+                    <Heading>Bet Privacy</Heading>
+                    <Text mb={1}>Is your bet public or private?</Text>
+                    <RadioGroup onChange={setBetPrivacy} value={betPrivacy} mb={4}>
+                        <Stack direction='row'>
+                            <Radio value='1' colorScheme="pink">Public</Radio>
+                            <Radio value='2' colorScheme="pink">Private</Radio>
+                        </Stack>
+                    </RadioGroup>
+                </Flex>
+                <Flex direction="column" justify="center" align="center">
+                    <Heading>Bet Side</Heading>
+                    <Text mb={1}>Which side of the bet are you on?</Text>
+                    <RadioGroup onChange={setBetSide} value={betSide} mb={4}>
+                        <Stack direction='row'>
+                            <Radio value='1' colorScheme="pink">Affirmation</Radio>
+                            <Radio value='2' colorScheme="pink">Negation</Radio>
+                        </Stack>
+                    </RadioGroup>
+                </Flex>
+            </Flex>
             <Heading>Counter Bet Size</Heading>
             <NumberInput size='sm' bg="whiteAlpha.700" color="#525252" defaultValue={0} min={0} mb={4}>
                 <NumberInputField focusBorderColor='red.200' />
